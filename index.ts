1ce6a7165a5aa592a8edfd796cd6c54256643abe
Sergei Smirnov <sergous@gmail.com>
2024-08-27 03:49:49 -0400

    Setup Prisma withAccelerate

--- index.ts
+++ index.ts
@@ -1,26 +1,38 @@
-import 'dotenv/config'
-import { PrismaClient } from '@prisma/client'
-import { withPulse } from '@prisma/extension-pulse'
+import "dotenv/config";
+import { PrismaClient } from "@prisma/client";
+import { withPulse } from "@prisma/extension-pulse";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
-process.on('SIGINT', () => {
-  process.exit(0)
-})
+process.on("SIGINT", () => {
+  process.exit(0);
+});
 
-const apiKey: string = process.env.PULSE_API_KEY ?? ''
-const prisma = new PrismaClient().$extends(
-  withPulse({ apiKey: apiKey })
-)
+const apiKey: string = process.env.PULSE_API_KEY ?? "";
+const prisma = new PrismaClient()
+  .$extends(withPulse({ apiKey: apiKey }))
+  .$extends(withAccelerate());
 
 async function main() {
-  const stream = await prisma.user.stream()
+  const foundUsers = await prisma.user.findMany({
+    where: {
+      email: {
+        contains: "test",
+      },
+    },
+    cacheStrategy: { ttl: 60 },
+  });
 
-  process.on('exit', (code) => {
-    stream.stop()
-  })
+  console.log("found users", foundUsers);
+
+  const stream = await prisma.user.stream();
+
+  process.on("exit", (code) => {
+    stream.stop();
+  });
 
   for await (const event of stream) {
-    console.log('just received an event:', event)
+    console.log("just received an event:", event);
   }
 }
 
-main()
+main();
